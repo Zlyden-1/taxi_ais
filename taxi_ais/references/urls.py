b74926ce6fcc0503029131cd752ee3b6ba0175c4
Максим Авдошин <Zlyden-1@yandex.ru>
2023-03-24 18:04:07 +0400

    Added CRUD functions and UI for contractors

--- taxi_ais/references/urls.py
+++ taxi_ais/references/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
-
 app_name = 'contractors'
 urlpatterns = [
     path('contractors/', views.contractors, name='Contractors'),
-    path('add_contractor/', views.add_contractor, name='add_contractor')
+    path('add_contractor/', views.add_contractor, name='add_contractor'),
+    path('contractor/<int:pk>', views.contractor_detail, name='contractor'),
+    path('contractor/<int:pk>/edit', views.edit_contractor, name='edit_contractor'),
+    path('contractor/<int:pk>/delete', views.delete_contractor, name='delete_contractor'),
 ]
--- taxi_ais/references/views.py
+++ taxi_ais/references/views.py
@@ -24,3 +24,34 @@ def add_contractor(request):
     else:
         new_contractor.save()
         return HttpResponseRedirect(redirect_to='/references/contractors')
+
+
+def contractor_detail(request, pk):
+    contractor = Contractor.objects.get(pk=pk)
+    drivers = Driver.objects.all()
+    vehicles = Vehicle.objects.all()
+    context = {'contractor': contractor,
+               'drivers': drivers,
+               'vehicles': vehicles}
+    return render(request, 'references/contractor.html', context)
+
+
+def edit_contractor(request, pk):
+    try:
+        contractor = Contractor.objects.get(pk=pk)
+        contractor.driver = Driver.objects.get(pk=request.POST['driver'])
+        contractor.vehicle = Vehicle.objects.get(pk=request.POST['vehicle'])
+        contractor.renting_date = request.POST['renting_date']
+    except KeyError:
+        return render(request, 'references/error.html', {
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        contractor.save()
+        return HttpResponseRedirect(redirect_to='/references/contractors')
+
+
+def delete_contractor(request, pk):
+    contractor = Contractor.objects.get(pk=pk)
+    contractor.delete()
+    return HttpResponseRedirect(redirect_to='/references/contractors')
